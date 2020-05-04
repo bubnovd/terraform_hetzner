5e03249a52127025032cc076175b96d20b89696e
Dmitry Bubnov <dv@bubnovd.net>
2020-05-04 11:53:34 +0600

    init commit

---
+++ hcloud.tf
@@ -0,0 +1,25 @@
+# Set the variable value in *.tfvars file
+# or using the -var="hcloud_token=..." CLI option
+variable "hcloud_token" {}
+variable "hcloud_ssh_key_fingerprint" {}
+
+# Configure the Hetzner Cloud Provider
+provider "hcloud" {
+  token = "${var.hcloud_token}"
+}
+
+data "hcloud_ssh_key" "ssh_key" {
+  fingerprint = "${var.hcloud_ssh_key_fingerprint}"
+}
+
+# Create a server
+resource "hcloud_server" "test" {
+  name        = "test-srv"
+  image       = "debian-10"
+  server_type = "cx11"
+  ssh_keys    = ["${data.hcloud_ssh_key.ssh_key.id}"]
+}
+
+output "server_ip" {
+  value = "${hcloud_server.test.ipv4_address}"
+}
